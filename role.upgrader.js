b0588962bac29ff9e277734554ac65334f1854bd
jjsosa <sosaosuna@gmail.com>
2020-04-02 01:39:46 +0200

    Initial tutorial script (harvesters and upgraders automatically spawning)

--- role.upgrader.js
+++ role.upgrader.js
@@ -1,7 +1,7 @@
 module.exports = {
     // a function to run the logic for this role
     run: function(creep) {
-        // if creep is bringing energy to the spawn but has no energy left
+        // if creep is bringing energy to the controller but has no energy left
         if (creep.memory.working == true && creep.carry.energy == 0) {
             // switch state
             creep.memory.working = false;
@@ -12,12 +12,15 @@ module.exports = {
             creep.memory.working = true;
         }
 
-        // if creep is supposed to transfer energy to the spawn
+        // if creep is supposed to transfer energy to the controller
         if (creep.memory.working == true) {
-            // try to transfer energy, if the spawn is not in range
-            if (creep.transfer(Game.spawns.Spawn1, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-                // move towards the spawn
-                creep.moveTo(Game.spawns.Spawn1);
+            // instead of upgraderController we could also use:
+            // if (creep.transfer(creep.room.controller, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+
+            // try to upgrade the controller
+            if (creep.upgradeController(creep.room.controller) == ERR_NOT_IN_RANGE) {
+                // if not in range, move towards the controller
+                creep.moveTo(creep.room.controller);
             }
         }
         // if creep is supposed to harvest energy from source
